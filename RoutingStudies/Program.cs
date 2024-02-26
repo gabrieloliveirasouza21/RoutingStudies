1d5988aea9f1be101486fad30a1811f8106dfbd4
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2024-02-25 22:50:26 -0300

    Aula sobre Map() ; MapGet() ; MapPost()

--- RoutingStudies/Program.cs
+++ RoutingStudies/Program.cs
@@ -11,7 +11,25 @@ app.UseRouting();
 //aqui é onde se cria os endpoints usando Map() ; MapGet() ou MapPost().
 app.UseEndpoints(endpoints =>
 {
+    endpoints.Map("homePage", async (HttpContext context) =>
+    {
+        await context.Response.WriteAsync("HomePage aqui");
+    });
 
+    endpoints.MapGet("aboutPage", async (HttpContext context) =>
+    {
+        await context.Response.WriteAsync("About page com metodo GET");
+    });
+
+    endpoints.MapPost("contactsPage", async (HttpContext context) =>
+    {
+        await context.Response.WriteAsync("Contacts Page com metodo POST");
+    });
+});
+
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync($"Pagina chamada na seguinte URL : {context.Request.Path}");
 });
 
 app.Run();
